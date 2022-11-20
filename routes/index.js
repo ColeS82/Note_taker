a259cb738f4359f65ba5a6ac4ae2c0f58c076f64
C-dub <25037768+ColeS82@users.noreply.github.com>
2022-11-20 16:02:44 -0700

    added gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
 // Import our modular routers for /tips and /feedback
-const homeRouter = require('./home');
+
 const notesRouter = require('./notes');
 
 const app = express();
 
-app.use('/home', homeRouter);
-app.use('/noes', notesRouter);
+
+app.use('/notes', notesRouter);
 
 module.exports = app;
--- routes/notes.js
+++ routes/notes.js
@@ -1,32 +1,29 @@
 const notes = require('express').Router();
-const { readAndAppend } = require('../helpers/fsUtils');
-const uuid = require('../helpers/uuid');
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 
 // GET Route for retrieving all the feedback
 notes.get('/', (req, res) =>
-  readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)))
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
 // POST Route for submitting feedback
 notes.post('/', (req, res) => {
   // Destructuring assignment for the items in req.body
-  const { email, feedbackType, feedback } = req.body;
+  const { note, noteTitle } = req.body;
 
   // If all the required properties are present
-  if (email && feedbackType && feedback) {
+  if (note && noteTitle) {
     // Variable for the object we will save
-    const newFeedback = {
-      email,
-      feedbackType,
-      feedback,
-      feedback_id: uuid(),
+    const newNote = {
+      note,
+      noteTitle,
     };
 
-    readAndAppend(newFeedback, './db/feedback.json');
+    readAndAppend(newNote, './db/db.json');
 
     const response = {
       status: 'success',
-      body: newFeedback,
+      body: newNote,
     };
 
     res.json(response);
